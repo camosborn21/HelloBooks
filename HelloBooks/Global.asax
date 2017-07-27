4f1089e5c7d9c8b6b9d864d59990d31f0410eed9
Cameron Osborn <camosborn21@msn.com>
2017-07-26 22:54:52 -0700

    Add project files.

---
+++ HelloBooks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloBooks.MvcApplication" Language="C#" %>
---
+++ HelloBooks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloBooks.Startup))]
+namespace HelloBooks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
